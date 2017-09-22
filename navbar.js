f6eb57b8ff95fdbbc96807890438648c629a8f76
waltersdavid <30503434+waltersdavid@users.noreply.github.com>
2017-09-22 13:23:48 -0500

    Add files via upload
    changed text

---
+++ navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        changed the navbar to test - tyler was here - but David came after and was better
+      </div>
+    )
+  }
+}
+
+export default Navbar;
